9d8b6508c8b4b402bab9e4c4d640aec696656526
hitYOUcry <285692817@qq.com>
2017-12-12 23:21:27 +0800

    upgrade script

--- groovyTest/hello.groovy
+++ groovyTest/hello.groovy
@@ -1,5 +1,6 @@
 println "hello"
 
+// ÀàÓ¦ÓÃ×Ö·û´®
 class Person {
 	String name
 	Person(String self){
@@ -10,13 +11,11 @@ class Person {
 		 "hello ${otherPerson},I am ${name}"
 	}
 }
-
 Person nemo = new Person("nemo");
 println nemo.greet("tunes")
 println nemo.name
 println nemo.'name'
 println nemo."name"
-
 def aMultilineString = '''line one
 line two
 line three'''
@@ -29,3 +28,26 @@ line three
 print aMultilineString
 
 println 'The Euro currency symbol: \u20AC'
+
+def sum = "The sum of 2 and 3 equals ${2 + 3}"
+println sum
+
+
+
+def person = [name: 'Guillaume', age: 36]
+println "$person.name is $person.age years old"
+
+println "\$person.age"
+
+def number = 1 
+def eagerGString = "value == ${number}"
+def lazyGString = "value == ${ -> number }"
+println eagerGString
+println lazyGString
+
+number = 2 
+println eagerGString
+println lazyGString
+
+
+
